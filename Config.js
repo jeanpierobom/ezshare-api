ec47f053ab7db9cc2dc9934fbd73f2ee77059941
jeanpierobom <pierobom@gmail.com>
2019-02-27 17:07:43 -0800

    Adjustments in the API

---
+++ Config.js
@@ -0,0 +1,5 @@
+var Config = {
+    USER_ID_EZGAMEPLAYS: 'us-east-1:220df5a3-32b2-4a35-853f-b447779631a7'
+}
+
+export default Config
--- list.js
+++ list.js
@@ -1,5 +1,6 @@
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
+import Config from './Config'
 
 export async function main(event, context) {
   const params = {
@@ -10,9 +11,11 @@ export async function main(event, context) {
     // 'ExpressionAttributeValues' defines the value in the condition
     // - ':userId': defines 'userId' to be Identity Pool identity id
     //   of the authenticated user
+
     KeyConditionExpression: "userId = :userId",
     ExpressionAttributeValues: {
-      ":userId": event.requestContext.identity.cognitoIdentityId
+      //":userId": event.requestContext.identity.cognitoIdentityId
+      ":userId": Config.USER_ID_EZGAMEPLAYS
     }
   };
 
